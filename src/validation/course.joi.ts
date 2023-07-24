64caacfa228c9357f6bdf70d0dc151504ad52095
tung <tungngo1107@gmail.com>
2023-07-24 17:46:51 +0700

    require? ()

--- src/validation/course.joi.ts
+++ src/validation/course.joi.ts
@@ -4,20 +4,20 @@ import { ICourse } from "@/models/course.model";
 export const CourseSchema = {
     Course: {
         create_course: Joi.object<ICourse>({
-            workplace: Joi.string().max(50).required,
-            course_code: Joi.string().max(10).required,
-            title: Joi.string().max(20).required,
-            image: Joi.string().max(200).required,
+            workplace: Joi.string().max(50).required(),
+            course_code: Joi.string().max(10).required(),
+            title: Joi.string().max(20).required(),
+            image: Joi.string().max(200).required(),
             session_per_course: Joi.string().max(5).required(),
-            price: Joi.string().max(10).required,
+            price: Joi.string().max(10).required(),
         }),
         update_course: Joi.object<ICourse>({
-            workplace: Joi.string().max(50).required,
-            course_code: Joi.string().max(10).required,
-            title: Joi.string().max(20).required,
-            image: Joi.string().max(200).required,
+            workplace: Joi.string().max(50).required(),
+            course_code: Joi.string().max(10).required(),
+            title: Joi.string().max(20).required(),
+            image: Joi.string().max(200).required(),
             session_per_course: Joi.string().max(5).required(),
-            price: Joi.string().max(10).required,
+            price: Joi.string().max(10).required(),
         })
     }
 }
